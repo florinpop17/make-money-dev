45cd09040683137fd6c89cab4d7a896c1bc2bf6f
Florin Pop <popflorin1705@yahoo.com>
2021-02-25 17:50:55 +0200

    added check for browser scripts

--- components/Layout.js
+++ components/Layout.js
@@ -3,25 +3,34 @@ import Head from "./Head";
 import Nav from "./Nav";
 import Footer from "./Footer";
 
+const isBrowser = typeof window !== "undefined";
+
 const Layout = ({ children }) => {
     const [darkTheme, setDarkTheme] = React.useState(true);
 
     return (
         <div className={darkTheme ? "dark" : ""}>
             <Head>
-                <script async src="https://f.convertkit.com/ckjs/ck.5.js" />
-                <script
-                    async
-                    defer
-                    data-domain="makemoney.dev"
-                    src="https://plausible.io/js/plausible.js"
-                ></script>
-                <script
-                    async
-                    defer
-                    data-domain="makemoney.dev"
-                    src="https://plausible.io/js/plausible.outbound-links.js"
-                ></script>
+                {isBrowser && (
+                    <>
+                        <script
+                            async
+                            src="https://f.convertkit.com/ckjs/ck.5.js"
+                        />
+                        <script
+                            async
+                            defer
+                            data-domain="makemoney.dev"
+                            src="https://plausible.io/js/plausible.js"
+                        ></script>
+                        <script
+                            async
+                            defer
+                            data-domain="makemoney.dev"
+                            src="https://plausible.io/js/plausible.outbound-links.js"
+                        ></script>
+                    </>
+                )}
             </Head>
             <div className="flex flex-col min-h-screen bg-white dark:bg-gray-800 p-4">
                 <div className="container mx-auto max-w-3xl lg:max-w-5xl">
